8e8d23e1a449a3a2dd001e641c7bca6cb7855b30
valderyjr <valderyjunior2002@gmail.com>
2022-06-04 20:00:24 -0300

    criando pagina que lista usuarios

--- src/pages/Usuarios/index.jsx
+++ src/pages/Usuarios/index.jsx
@@ -1,45 +1,68 @@
 import React, { useEffect, useState } from 'react'
+import Button from '../../components/Button';
 import api from '../../services/axios'
 import './Usuarios.css';
+import { AiFillDelete } from 'react-icons/ai'
+import { AiFillEdit } from 'react-icons/ai'
+import {HiUserAdd} from 'react-icons/hi'
 
 function Usuarios() {
 
 	const [usuarios, setUsuarios] = useState([])
 
-	useEffect(() => {
-		const obtemUsuarios = async () => {
-			try {
-				const response = await api.get('/usuarios')
-				const listaUsuarios = response.data
-				console.log(response.data)
-				setUsuarios([...listaUsuarios])
-			} catch (error) {
-				console.log(error);
-			}
+	const obtemUsuarios = async () => {
+		try {
+			const response = await api.get('/usuarios')
+			const listaUsuarios = response.data
+			console.log(response.data)
+			setUsuarios([...listaUsuarios])
+		} catch (error) {
+			console.log(error);
 		}
+	}
 
+	const deletarUsuario = async (id) => {
+		await api.delete(`/usuarios/${id}`)
+		obtemUsuarios()
+	}
+
+	useEffect(() => {
 		obtemUsuarios()
 	}, [])
+
 	return (
 		<main>
-			{/* <ul></ul> */}
+			<div className='usuario__novo'>
+				<Button adicionar={true} onClick={() => console.log('novo ein')}>
+				<HiUserAdd size='18px' style={{ marginRight: '5px' }} />
+					Adicionar um novo usuário
+				</Button>
+			</div>
+			<div className='usuarios'>
 			{usuarios.map(usuario => (
 				<div key={usuario.id} className='usuario'>
 					<div className='header'>
-						<div>{usuario.nome}</div>
+						<h2>{usuario.nome}</h2>
 					</div>
 					<div className='body'>
-						<div>{usuario.email}</div>
-						<div>{usuario.endereco}</div>
-						<div>{usuario.cidade}</div>
-						<div>{usuario.estado}</div>
+						<p><span>Email: </span>{usuario.email}</p>
+						<p><span>Endereço: </span>{usuario.endereco}</p>
+						<p><span>Cidade: </span>{usuario.cidade}, {usuario.estado}</p>
+						<p>{usuario.id}</p>
 					</div>
 					<div className='footer'>
-						<button>escolher</button>
-						<button>escolher</button>
+						<Button deletar={true} onClick={() => deletarUsuario(usuario.id)}>
+							<AiFillDelete color='white' size='18px' style={{ marginRight: '5px' }} />
+							Deletar
+						</Button>
+						<Button>
+							<AiFillEdit color='white' size='18px' style={{ marginRight: '5px' }} />
+							Editar
+						</Button>
 					</div>
 				</div>
 			))}
+			</div>
 		</main>
 	)
 }
