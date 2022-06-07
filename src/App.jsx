172558929df2dc910949ccb42272900bf5211c9c
Nando Merces <nando.merces@gmail.com>
2022-06-07 20:47:56 -0300

    Atualização Funcionário

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import Usuarios from './pages/Usuarios'
+import Funcionario from './pages/Funcionario'
 import Home from './pages/Home'
 import NovoUsuario from './pages/Usuarios/NovoUsuario'
+import NovoFuncionario from './pages/Funcionario/NovoFuncionario'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import EditarUsuario from './pages/Usuarios/EditarUsuario'
-import Footer from './components/Footer'
+import EditFuncionario from './pages/Funcionario/EditFuncionario'
 
 function App() {
 
@@ -18,6 +20,12 @@ function App() {
 					<Route path='/usuarios' element={<Usuarios />} />
 					<Route path='/usuarios/novo' element={<NovoUsuario />} />
 					<Route path='/usuarios/:id' element={<EditarUsuario />} />
+				</Routes>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/funcionario' element={<Funcionario />} />
+					<Route path='/funcionario/novo' element={<NovoFuncionario />} />
+					<Route path='/funcionario/:id' element={<EditFuncionario />} />
 				</Routes>
 				<Footer />
 			</BrowserRouter>
