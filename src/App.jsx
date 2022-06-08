d3a26fe466f1937a5f6b5b524a1b7505b8cb2eec
MatricBts <matricbts@gmail.com>
2022-06-07 21:28:49 -0300

    adicionando rota de Pecas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 import Usuarios from './pages/Usuarios'
+import Pecas from './pages/Pecas'
+import EditarPeca from './pages/Pecas/EditarPeca'
 import Home from './pages/Home'
 import NovoUsuario from './pages/Usuarios/NovoUsuario'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -18,6 +20,8 @@ function App() {
 					<Route path='/usuarios' element={<Usuarios />} />
 					<Route path='/usuarios/novo' element={<NovoUsuario />} />
 					<Route path='/usuarios/:id' element={<EditarUsuario />} />
+					<Route path='/pecas' element={<Pecas />}></Route>
+					<Route path='/pecas/:id' element={<EditarPeca />}></Route>
 				</Routes>
 				<Footer />
 			</BrowserRouter>
