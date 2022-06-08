37661954c4b093d87881b5d07758017949db569c
AlexGo2 <alex.oliveira29@hotmail.com>
2022-06-08 13:02:25 -0300

    fornecedor, get, update, delete

--- src/pages/Fornecedores/index.jsx
+++ src/pages/Fornecedores/index.jsx
@@ -1,25 +1,62 @@
-import {React} from "react";
+import { React, useEffect, useState } from "react";
 import api from "../../services/axios";
 
-export default () => { 
 
-    
 
-    const ListaFornecer = async () => {
+export default () => {
+    const [fornecedor, setFornecedor] = useState([])
 
-		const req = await api.get('/fornecedor');
-		const res =  req.data;
-        console.log(res);		
+
+    const GetFornecer = async () => {
+
+        const req = await api.get('/fornecedor');
+        const res = req.data;
+        setFornecedor([...res]);
+    }
+
+    const DeleteFornecer = async (id) => {
+
+        await api.delete(`/usuarios/${id}`);
+		GetFornecer(); 
+    }
+
+    const UpdateFornecer = (id) => {
+		navigate(`${id}`)
 	}
+   
+
+    useEffect(() => {
+        GetFornecer();
+    }, [])
 
 
-
-
-    return ( 
-       <main>
-           <h1>Lista de Fornecedores</h1>
-           <input type="button" value="lista" onClick={()=>{ListaFornecer()}} />
-
-       </main>
+    return (
+        <main>
+            <h1>Lista de Fornecedores</h1>
+            <table>
+                <tr>
+                    <td>Id</td>
+                    <td>Fornecedor</td>
+                    <td>Marca</td>
+                    <td>Pedido</td>
+                    <td>Qtd</td>
+                    <td>Vencimento</td>
+                </tr>
+                {fornecedor.map((fornecedor, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{fornecedor.id}</td>
+                            <td>{fornecedor.nome}</td>
+                            <td>{fornecedor.marca}</td>
+                            <td>{fornecedor.pedido}</td>
+                            <td>{fornecedor.qtd}</td>
+                            <td>{fornecedor.vencimento}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+            
+            
+        </main>
     )
 }
